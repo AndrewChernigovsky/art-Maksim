00095e110f52b04539b78473d3d3d9f7ba221394
Korsun112 <cs16maksim@gmail.com>
2024-03-19 20:41:59 +0500

    added script filters cards

--- source/js/burger.js
+++
@@ -1,8 +0,0 @@
-const navBurger = document.querySelector('.nav__burger');
-const navList = document.querySelector('.nav__list');
-const navCart = document.querySelector('.nav__cart');
-
-navBurger.addEventListener('click', () => {
-  navCart.classList.toggle('nav__cart--unvisible');
-  navList.classList.toggle('nav__list--visible');
-});
